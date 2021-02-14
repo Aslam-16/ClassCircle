520c917cbe7bf5fabee1e3d8c9568accd1e7bc29
naren67 <75604512+naren67@users.noreply.github.com>
2021-02-14 18:49:50 +0530

    Add files via upload

---
+++ typecode.ts
@@ -0,0 +1,42 @@
+interface GivenDetails {
+    radius: number;
+    color: string;
+}
+
+class Circle {
+    CircleRadius: number;
+    CircleColor: string;
+    CircleArea: number;
+    CircleCircumference: number;
+tsc 
+    constructor(object: GivenDetails) {
+        this.CircleRadius = object.radius;
+        this.CircleColor = object.color;
+    }
+
+    getRadius() {
+        console.log(this.CircleRadius);
+    }
+
+    getColor() {
+        console.log(this.CircleColor);
+    }
+
+    getArea() {
+        console.log(this.CircleArea);
+    }
+
+    getCircumference() {
+        console.log(this.CircleCircumference);
+    }
+
+}
+
+let object: GivenDetails = {
+    radius: 1.1,
+    color: 'red',
+}
+let Data = new Circle(object);
+Data.getColor();
+Data.getRadius();
+
